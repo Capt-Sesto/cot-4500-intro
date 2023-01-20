076668222bea00f1c336d805ca53aaac0232fa17
Capt-Sesto <122383706+Capt-Sesto@users.noreply.github.com>
2023-01-20 18:50:10 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,1 +1,28 @@
+import numpy as np
+
+def itp_arr():
+    arr = np.array(([0,0,0],[0,0,0],[0,0,0]))
+    for s in range(0,3):
+        for m in range(0,3):
+            if s == m:
+                arr[s][m] = 1
+            else:
+                arr[s][m] = 0
+    print(arr)
+    print("\n")
+
+    for s in range(0,3):
+        for m in range(0,3):
+            if s == m:
+                arr[s][m] = 1
+            else:
+                arr[s][m] = 3
+    print(arr)
+    print("\n")
+
+    arr = np.delete(arr, 2, 1)
+    print(arr)
+
+if __name__ == "__main__":
+    itp_arr()
 
